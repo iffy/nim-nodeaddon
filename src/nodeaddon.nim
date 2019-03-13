c19af834f97217d3e56332819a03128006dc66a0
Matt Haggard <haggardii@gmail.com>
2019-03-13 15:26:26 -0600

    Try trusty

--- src/nodeaddon.nim
+++ src/nodeaddon.nim
@@ -1,8 +1,3 @@
-# This is just an example to get you started. A typical hybrid package
-# uses this file as the main entry point of the application.
-
-import nodeaddonpkg/lowlevel
-
 when isMainModule:
   echo "NOT YET IMPLEMENTED"
   quit(1)
