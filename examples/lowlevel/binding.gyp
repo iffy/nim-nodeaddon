303f230c080a77898a1937beee8a3b7dbd0a295c
Matt Haggard <haggardii@gmail.com>
2019-03-14 11:59:37 -0600

    Maybe fix linux build

--- examples/lowlevel/binding.gyp
+++ examples/lowlevel/binding.gyp
@@ -5,6 +5,8 @@
             "sources": [
                 "<!@(node -p \"require('fs').readdirSync('./native/csrc').map(f=>'native/csrc/'+f).join(' ')\")",
             ],
+            'cflags!': [ '-fno-exceptions' ],
+            'cflags_cc!': [ '-fno-exceptions' ],
             'xcode_settings': {
                 'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
                 'CLANG_CXX_LIBRARY': 'libc++',
--- nakefile.nim
+++ nakefile.nim
@@ -5,3 +5,12 @@ import nake
 task "functest", "Run the functional tests":
   withDir("examples"/"lowlevel"):
     direShell "nake", "test"
+
+const linuxbuildertag = "nimnodeaddon:builder"
+
+task "dockerbuild", "Create a Travis-like docker image for testing linux":
+  direShell "docker", "build", "--file", "linux.Dockerfile", "--tag", linuxbuildertag, "."
+
+task "linuxtest", "Run tests in linux container":
+  runTask "dockerbuild"
+  direShell "docker", "run", "--rm", "-it", "-v", getCurrentDir() & ":/code", linuxbuildertag, "./runtests.sh"
